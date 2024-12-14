05c8c6241366dbcef4367cfeaa54057e431a77d0
Elddar00 <eldaridrizovic00@gmail.com>
2024-12-14 22:29:08 +0100

    React - Composable

--- react-facts2.0/src/index.js
+++ react-facts2.0/src/index.js
@@ -1,9 +1,26 @@
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <ul>
-    <li>Super popular JS library</li>
-    <li>Will help me be even more employable</li>
-    <li>React has a great logo</li>
-  </ul>
+// createRoot(document.getElementById("root")).render(
+//   <ul>
+//     <li>Super popular JS library</li>
+//     <li>Will help me be even more employable</li>
+//     <li>React has a great logo</li>
+//   </ul>
+// );
+
+// const root = createRoot(document.getElementById("root"));
+// const reactElement = <h1>Hello from JSX</h1>;
+// console.log(reactElement);
+// root.render(reactElement);
+
+const root = createRoot(document.getElementById("root"));
+
+function MainContent() {
+  return <h1>Hello React, React is awesome</h1>;
+}
+
+root.render(
+  <div>
+    <MainContent />
+  </div>
 );
