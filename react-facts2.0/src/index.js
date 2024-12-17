005bcffadf2ea74865925d1928ccf34c80e51487
Elddar00 <eldaridrizovic00@gmail.com>
2024-12-17 20:06:48 +0100

    Styling with classes

--- react-facts2.0/src/index.js
+++ react-facts2.0/src/index.js
@@ -65,8 +65,15 @@ const root = createRoot(document.getElementById("root"));
 
 function Header() {
   return (
-    <header>
-      <img src="logo512.png" width="40px" alt="React logo" />
+    <header className="header">
+      <img src="logo512.png" className="nav-logo" alt="React logo" />
+      <nav>
+        <ul className="nav-list">
+          <li className="nav-list-item">Pricing</li>
+          <li className="nav-list-item">About</li>
+          <li className="nav-list-item">Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 }
