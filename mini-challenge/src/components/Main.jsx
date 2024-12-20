ab968b6c436aeb15e521be5f555307fb5d1a9018
Elddar00 <eldaridrizovic00@gmail.com>
2024-12-20 19:58:55 +0100

    Main content section

--- mini-challenge/src/components/Main.jsx
+++ mini-challenge/src/components/Main.jsx
@@ -1,5 +1,16 @@
 import "../App.css";
 
 export default function Main() {
-  return <div>Main components herere</div>;
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul className="facts-list">
+        <li>Was first realesed in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 }
