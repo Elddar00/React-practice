056f4b82f8d044a89335f4bbe4b0b765c29cb1c3
Elddar00 <eldaridrizovic00@gmail.com>
2024-12-31 21:53:06 +0100

    Travel Journal: Key Prop

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
 import Header from "./components/Header";
 import Entry from "./components/Entry";
+import data from "./data";
 
 function App() {
+  const entryElements = data.map((entry) => {
+    return (
+      <Entry
+        key={entry.id}
+        img={entry.img}
+        title={entry.title}
+        country={entry.country}
+        googleMapsLink={entry.googleMapsLink}
+        dates={entry.dates}
+        text={entry.text}
+      />
+    );
+  });
   return (
     <>
       <Header />
-      <main className="container">
-        <Entry />
-      </main>
+      <main className="container">{entryElements}</main>
     </>
   );
 }
--- travel-journal/src/components/Entry.jsx
+++ travel-journal/src/components/Entry.jsx
@@ -1,26 +1,19 @@
-export default function Entry() {
+export default function Entry(props) {
+  //   console.log(prop);
   return (
     <article className="journal-entry">
       <div className="main-image-container">
-        <img
-          className="main-image"
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-          alt="mount fuji"
-        />
+        <img className="main-image" src={props.img.src} alt={props.img.alt} />
       </div>
       <div className="info-container">
         <img className="marker" src="/marker.png" alt="map marker icon" />
-        <span className="country">Japan</span>
-        <a href="https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9">
+        <span className="country">{props.country}</span>
+        <a href={props.googleMapsLink} target="_blank">
           View on Google Maps
         </a>
-        <h2 className="entry-title">Mount Fuji</h2>
-        <p className="trip-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className="entry-text">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
-        </p>
+        <h2 className="entry-title">{props.title}</h2>
+        <p className="trip-dates">{props.dates}</p>
+        <p className="entry-text">{props.text}</p>
       </div>
     </article>
   );
