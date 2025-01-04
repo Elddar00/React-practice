2fd27c3e875e2375b28feccfecaf171fa8f8cef0
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-04 21:15:05 +0100

    Chef Claude: Header

---
+++ chef-claude/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <img src="/chef-claude-icon.png" alt="" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
+
+export default Header;
