567d93b82477fec66657244d43da7557cd5a89bc
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-08 19:45:08 +0100

    Chef Claude: map ingredients list

--- chef-claude/src/Main.jsx
+++ chef-claude/src/Main.jsx
@@ -1,14 +1,31 @@
 export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Form submitted!");
+    const formData = new FormData(e.currentTarget);
+    const newIngredients = formData.get("ingredient");
+    ingredients.push(newIngredients);
+    console.log(ingredients);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add Ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 }
