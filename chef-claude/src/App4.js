a489c9bee087d4710b055958bcabaaa827a9102a
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-10 18:39:53 +0100

    complex state: arrays

---
+++ chef-claude/src/App4.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function App4() {
+  /**
+   * Challenge: Convert the code below to use an array
+   * held in state instead of a local variable. Initialize
+   * the state array as an empty array
+   *
+   * Don't worry about fixing `addFavoriteThing` quite yet.
+   */
+  const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
+
+  const allFavoriteThings = [
+    "💦🌹",
+    "😺",
+    "💡🫖",
+    "🔥🧤",
+    "🟤🎁",
+    "🐴",
+    "🍎🥧",
+    "🚪🔔",
+    "🛷🔔",
+    "🥩🍝",
+  ];
+
+  const thingsElements = myFavoriteThings.map((thing) => (
+    <p key={thing}>{thing}</p>
+  ));
+
+  function addFavoriteThing() {
+    setMyFavoriteThings((prevFavThings) => [
+      ...prevFavThings,
+      allFavoriteThings[prevFavThings.length],
+    ]);
+  }
+
+  return (
+    <main>
+      <button onClick={addFavoriteThing}>Add item</button>
+      <section aria-live="polite">{thingsElements}</section>
+    </main>
+  );
+}
