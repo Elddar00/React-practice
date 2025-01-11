94e3839330e4b32bfde85ff20082f91083f54db2
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-11 19:00:21 +0100

    Complex state: Objects

---
+++ chef-claude/src/Card.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./card.css";
+
+export default function Card() {
+  const avatar = "/user.png";
+  const starFilled = "/star-filled.png";
+  const starEmpty = "/star-empty.png";
+
+  const [contact, setContact] = React.useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (212) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: true,
+  });
+
+  let starIcon = contact.isFavorite ? starFilled : starEmpty;
+
+  function toggleFavorite() {
+    console.log("Toggle Favorite");
+  }
+
+  return (
+    <main>
+      <article className="card">
+        <img
+          src={avatar}
+          className="avatar"
+          alt="User profile picture of John Doe"
+        />
+        <div className="info">
+          <button
+            onClick={toggleFavorite}
+            aria-pressed={contact.isFavorite}
+            aria-label={
+              contact.isFavorite ? "Remove from favorites" : "Add to favorites"
+            }
+            className="favorite-button"
+          >
+            <img
+              src={starIcon}
+              alt={contact.isFavorite ? "filled star icon" : "empty star icon"}
+              className="favorite"
+            />
+          </button>
+          <h2 className="name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="contact">{contact.phone}</p>
+          <p className="contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  );
+}
--- chef-claude/src/index.js
+++ chef-claude/src/index.js
@@ -4,12 +4,14 @@ import App from "./App";
 import App2 from "./App2";
 import App3 from "./App3";
 import App4 from "./App4";
+import Card from "./Card";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <App2 /> */}
     {/* <App3 /> */}
     {/* <App4 /> */}
+    <Card />
   </React.StrictMode>
 );
