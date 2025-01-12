983d58cd13fdad5f2621b1f3fcf5514a8b0a2f7d
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-12 21:37:46 +0100

    form submission

--- chef-claude/src/Practice.jsx
+++ chef-claude/src/Practice.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./practice.css";
 
 /**
  * Challenge: add another label and input for the password field
  */
 
 export default function Practice() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget;
+    const formData = new FormData(formEl);
+    const email = formData.get("email");
+    // Gather the info from the form
+    // Submit it to a backend
+    formEl.reset();
+  }
+
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
+      <form onSubmit={handleSubmit} method="post">
         <label htmlFor="email">Email:</label>
         <input
           id="email"
