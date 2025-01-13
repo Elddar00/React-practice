9ec860acc14e24bc099b1221bce8636f30a50474
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-13 19:57:06 +0100

    forms: radio

--- chef-claude/src/Practice.jsx
+++ chef-claude/src/Practice.jsx
@@ -10,7 +10,9 @@ export default function Practice() {
   function signUp(formData) {
     const email = formData.get("email");
     const password = formData.get("password");
+    const employmentStatus = formData.get("employmentStatus");
     // console.log(email, password);
+    console.log(employmentStatus);
   }
 
   return (
@@ -31,6 +33,29 @@ export default function Practice() {
         <br />
         <label htmlFor="description">Description:</label>
         <textarea id="description" name="description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label htmlFor="description">
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+          </label>
+
+          <label htmlFor="">
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+          </label>
+
+          <label htmlFor="">
+            <input
+              type="radio"
+              name="employmentStatus"
+              defaultChecked={true}
+              value="full-time"
+            />
+            Full-time
+          </label>
+        </fieldset>
 
         <button>Submit</button>
       </form>
