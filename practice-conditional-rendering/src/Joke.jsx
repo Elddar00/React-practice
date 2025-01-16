7dcd2e36d7ebda67d14a399cad3e2c49130f215b
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-16 21:15:20 +0100

    Practice: &&

--- practice-conditional-rendering/src/Joke.jsx
+++ practice-conditional-rendering/src/Joke.jsx
@@ -3,16 +3,26 @@ import React from "react";
 export default function Joke(props) {
   const [isShown, setIsShown] = React.useState(false);
 
+  const [unreadMessage, setUnreadMessage] = React.useState(["a", "b"]);
+
   function toggleShown(prevBtn) {
     setIsShown((prevShown) => !prevShown);
   }
 
   return (
-    <div>
-      {props.setup && <h3>{props.setup}</h3>}
-      {isShown && <p>{props.punchline}</p>}
-      <button onClick={toggleShown}>Show punchline</button>
-      <hr />
-    </div>
+    <>
+      <div>
+        {props.setup && <h3>{props.setup}</h3>}
+        {isShown && <p>{props.punchline}</p>}
+        <button onClick={toggleShown}>Show punchline</button>
+        <hr />
+      </div>
+
+      <div>
+        {unreadMessage.length > 0 && (
+          <h1>You have {unreadMessage.length} unread messages!</h1>
+        )}
+      </div>
+    </>
   );
 }
