2e15e9dbb0f307af295039ee62616a441d2da65e
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-20 18:45:33 +0100

    Passing State as Props

--- chef-claude/src/index.js
+++ chef-claude/src/index.js
@@ -6,15 +6,17 @@ import App3 from "./App3";
 import App4 from "./App4";
 import Card from "./Card";
 import Practice from "./Practice";
+import Counter from "./Counter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
     {/* <App2 /> */}
     {/* <App3 /> */}
     {/* <App4 /> */}
     {/* <Card /> */}
     {/* <Practice /> */}
+    <Counter />
   </React.StrictMode>
 );
