3765b92f78e17fabd288ecb999ad116a6c9671bc
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-21 17:36:42 +0100

    Dynamic styles

--- sound-pads/src/App.js
+++ sound-pads/src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import "./App.css";
 import padsData from "./pads";
 
-export default function App() {
+export default function App({ darkMode }) {
   const [pads, setPads] = React.useState(padsData);
 
-  const buttonElements = pads.map((pad) => <button key={pad.id}></button>);
+  const styles = {
+    backgroundColor: darkMode ? "#222222" : "#cccccc",
+  };
+
+  const buttonElements = pads.map((pad) => (
+    <button style={styles} key={pad.id}></button>
+  ));
 
   return (
     <main>
