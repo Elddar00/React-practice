f29cafd4dbdfc7cf2b0f4ec786f749e60342ebdc
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-25 22:15:44 +0100

    Meme Generator: get a random meme

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -9,6 +9,25 @@ export default function Main() {
 
   const [allMemes, setAllMemes] = useState([]);
 
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getMemeImage() {
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const newMemeUrl = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      imageUrl: newMemeUrl,
+    }));
+    // get a random number from 0 to array.length
+    // use thath random number to get a random meme
+    // obj from the array
+    // Set state
+  }
+
   function handleChange(event) {
     const { value, name } = event.currentTarget;
     setMeme((prevMeme) => ({
@@ -16,12 +35,6 @@ export default function Main() {
       [name]: value,
     }));
   }
-
-  useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then((res) => res.json())
-      .then((data) => setAllMemes(data.data.memes));
-  }, []);
 
   return (
     <main>
@@ -47,7 +60,7 @@ export default function Main() {
             value={meme.bottomText}
           />
         </label>
-        <button>Get a new meme image 🖼</button>
+        <button onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
       <div className="meme">
         <img src={meme.imageUrl} />
