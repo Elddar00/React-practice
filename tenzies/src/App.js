18bf726245cc535355bb979fd782fd80c1e49239
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-26 20:31:53 +0100

    Tenzies: Generate 10 Random Numbers

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,6 +2,20 @@ import "./App.css";
 import Die from "./Die";
 
 function App() {
+  // const newDice = [];
+
+  //   for (let i = 0; i < 10; i++) {
+  //     const rand = Math.ceil(Math.random() * 6);
+  //     newDice.push(rand);
+  //   }
+  //   return newDice;
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  console.log(generateAllNewDice());
+
   return (
     <main>
       <div className="dice-container">
@@ -11,10 +25,10 @@ function App() {
         <Die value={4} />
         <Die value={5} />
         <Die value={6} />
-        <Die value={7} />
-        <Die value={8} />
-        <Die value={9} />
-        <Die value={10} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
       </div>
     </main>
   );
