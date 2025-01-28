62cfa19172d6809024531de1f75f6e84b11258ae
Elddar00 <eldaridrizovic00@gmail.com>
2025-01-28 19:27:34 +0100

    Tenzies: End game - confetti

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
   const [dice, setDice] = useState(generateAllNewDice());
@@ -54,6 +55,7 @@ function App() {
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
