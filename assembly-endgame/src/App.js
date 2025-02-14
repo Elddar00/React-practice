4db1567c48f3fb5b9979e0c43a6cd405008c5444
Elddar00 <eldaridrizovic00@gmail.com>
2025-02-14 16:45:17 +0100

    Assembly Endgame: Make the Game More Friendly

--- assembly-endgame/src/App.js
+++ assembly-endgame/src/App.js
@@ -142,10 +142,23 @@ export default function AssemblyEndgame() {
         </p>
       </header>
 
-      <section className={gameStatusClass}>{renderGameStatus()}</section>
+      <section aria-live="polite" role="status" className={gameStatusClass}>
+        {renderGameStatus()}
+      </section>
 
       <section className="language-chips">{langugeElements}</section>
       <section className="word">{letterElements}</section>
+      <section className="sr-only" aria-live="polite" role="status">
+        <p>
+          Current word:{" "}
+          {currentWord
+            .split("")
+            .map((letter) =>
+              guessedLetters.includes(letter) ? letter + "." : "blank."
+            )
+            .join(" ")}{" "}
+        </p>
+      </section>
       <section className="keyboard">{keyboardElements}</section>
       {isGameOver && <button className="new-game">New Game</button>}
     </main>
