e98215df66137ec644e0b23e2c28cf26d3d9a65a
Elddar00 <eldaridrizovic00@gmail.com>
2025-02-27 23:44:33 +0100

    List MovieList and implement Spinner

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -20,6 +21,8 @@ const App = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
@@ -33,10 +36,16 @@ const App = () => {
 
       if (data.Response === "False") {
         setErrorMessage(data.Error || "Failed to fetch movies");
+        setMovieList([]);
+        return;
       }
+
+      setMovieList(data.results || []);
     } catch (error) {
       console.log(`Error fetching movies: ${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -48,23 +57,35 @@ const App = () => {
     <main>
       <div className="pattern" />
 
-      <div className="wrapper" />
-      <header>
-        <img src="./hero.png" alt="Hero Banner" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
 
-        <h1 className="relative z-10">
-          Find <span className="text-gradient">Movies</span> You ll Enjoy
-          Without the Hassle
-        </h1>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      </header>
+          <h1 className="relative z-10">
+            Find <span className="text-gradient">Movies</span> You ll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
 
-      <section className="all-movies">
-        <h2 className="relative z-10">All Movies</h2>
+        <section className="all-movies">
+          <h2 className="mt-[40px] relative z-10">All Movies</h2>
 
-        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-      </section>
-      <div />
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
+              ))}
+            </ul>
+          )}
+        </section>
+      </div>
     </main>
   );
 };
