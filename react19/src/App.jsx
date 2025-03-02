f66e8af0a9f6b528a431d1addb88aeff48f45d3d
Elddar00 <eldaridrizovic00@gmail.com>
2025-03-02 23:30:43 +0100

    Implement Search

--- react19/src/App.jsx
+++ react19/src/App.jsx
@@ -23,11 +23,13 @@ const App = () => {
 
   // console.log(movieList);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -53,8 +55,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
